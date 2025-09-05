3616650702c287a27da396f0d5b6f36e4d76d47e
Severus Melvin <melvinmulyono02@gmail.com>
2025-09-05 17:40:21 +0700

    setup [SM] color

---
+++ lib/components/color/color.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static const Color primaryColor = Color(0xFFA62671);
+  static const Color secondaryColor = Color(0xFFDB466B);
+  static const Color accentColor = Color(0xFFE3B8D1);
+}
+
+class SupportColor {
+  static const Color supportColor = Color(0xFFF5D773);
+  static const Color supportColor2 = Color(0xFF8730D4);
+  static const Color supportColor3 = Color(0xFFE64792);
+  static const Color warningColor = Color(0xFFFF2929);
+  static const Color errorColor = Color(0xFFF96C48);
+  static const Color checkColor = Color(0xFF37D933);
+  static const Color grayColor = Color(0xFFA3A3A3);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
+
+class TextColor {
+  static const Color unfocusedColor = Color(0xFFA3A3A3);
+  static const Color unfocusedColor2 = Color(0xFFCCCCCC);
+  static const Color primaryTextColor = Color(0xFF420F2D);
+}
