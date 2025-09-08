573eed50666989ec35c57e26545fad7d80f9957f
Severus Melvin <melvinmulyono02@gmail.com>
2025-09-08 16:21:39 +0700

    bind [SM] bind pages

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,9 +1,14 @@
 import 'package:get/route_manager.dart';
+import 'package:ulangan1_11pplg2/bindings/login_binding.dart';
 import 'package:ulangan1_11pplg2/pages/login_page.dart';
 import 'package:ulangan1_11pplg2/routes/app_routes.dart';
 
 class AppPages {
   static final appPages = [
-    GetPage(name: AppRoutes.loginPage, page: () => LoginPage()),
+    GetPage(
+      name: AppRoutes.loginPage,
+      page: () => LoginPage(),
+      binding: LoginBinding(),
+    ),
   ];
 }
