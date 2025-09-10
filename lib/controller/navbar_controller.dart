ddc6bd8697cf384300505dfa4a6ff2e455141b1f
Severus Melvin <melvinmulyono02@gmail.com>
2025-09-10 12:42:45 +0700

    bind [SM] navbar & home page

--- lib/controller/navbar_controller.dart
+++ lib/controller/navbar_controller.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ulangan1_11pplg2/pages/home_page.dart';
 
 class NavbarController extends GetxController {
-  // var currentIndex = 0.obs;
+  var currentIndex = 0.obs;
 
-  final List<Widget> pages = [];
+  final List<Widget> pages = [HomePage()];
 
-  // void changeIndex(int index) {
-  //   currentIndex.value = index;
-  // }
+  void changeIndex(int index) {
+    currentIndex.value = index;
+  }
 }
