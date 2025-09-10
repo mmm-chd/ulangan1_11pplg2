ff0baa378d53475b4ad1a0b7fc2a2abe37a0bb70
Severus Melvin <melvinmulyono02@gmail.com>
2025-09-10 13:01:33 +0700

    feat [SM] home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,192 +12,209 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 60),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
-            RichText(
-            text: TextSpan(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SpacingComponent(height: 60),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextSpan(
-                  text: "Hello, ",
-                  style: TextStyle(
-                    color: TextColor.primaryTextColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
-                  )
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Hello, ",
+                        style: TextStyle(
+                          color: TextColor.primaryTextColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "Matthew !",
+                        style: TextStyle(
+                          color: MainColor.primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                TextSpan(
-                  text: "Matthew !",
-                  style: TextStyle(
-                    color: MainColor.primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
-                  )
+                SpacingComponent(height: 4),
+
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "You Have ",
+                        style: TextStyle(
+                          color: TextColor.primaryTextColor,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "15 tasks ",
+                        style: TextStyle(
+                          color: MainColor.primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "to complete\n",
+                        style: TextStyle(
+                          color: TextColor.primaryTextColor,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "this month.",
+                        style: TextStyle(
+                          color: TextColor.primaryTextColor,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ]
-            )
-            ),
 
-            SpacingComponent(height: 4),
+                SpacingComponent(height: 16),
 
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: "You Have ",
-                  style: TextStyle(
-                    color: TextColor.primaryTextColor,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
-                  )
+                CustomSearchBar(),
+                SpacingComponent(height: 20),
+
+                CustomText(
+                  text: "Categories",
+                  color: TextColor.primaryTextColor,
+                  weight: FontWeight.w700,
+                  size: 20,
                 ),
-                TextSpan(
-                  text: "15 tasks ",
-                  style: TextStyle(
-                    color: MainColor.primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  )
-                ),
-                TextSpan(
-                  text: "to complete\n",
-                  style: TextStyle(
-                    color: TextColor.primaryTextColor,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
-                  )
-                ),
-                TextSpan(
-                  text: "this month.",
-                  style: TextStyle(
-                    color: TextColor.primaryTextColor,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
-                  )
-                )
-              ]
-            )
-            ),
-
-            SpacingComponent(height: 16),
-
-            CustomSearchBar(),
-
-            SpacingComponent(height: 20),
-
-            CustomText(
-              text: "Categories", 
-              color: TextColor.primaryTextColor, 
-              weight: FontWeight.w700, 
-              size: 20),
-
-            SpacingComponent(height: 12),
-            
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-              children: [
-
-                CardviewComponent(
-                  color: PriorityColor.primaryColor, 
-                  color2: SupportColor.mustdopb,
-                  task: "0 Task", 
-                  title: "Must Do"),
-
-                  SizedBox(width: 12),
-
-                CardviewComponent(
-                  color: PriorityColor.secondaryColor, 
-                  color2: SupportColor.shoulddopb,
-                  task: "0 Task", 
-                  title: "Should Do"),
-
-                  SizedBox(width: 12),
-                
-                CardviewComponent(
-                  color: PriorityColor.accentColor, 
-                  color2: SupportColor.coulddopb,
-                  task: "0 Task", 
-                  title: "Could Do"),
-
-                  SizedBox(width: 12),
               ],
             ),
+          ),
+
+          SpacingComponent(height: 12),
+
+          SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                CardviewComponent(
+                  color: PriorityColor.primaryColor,
+                  color2: SupportColor.mustdopb,
+                  task: "0 Task",
+                  title: "Must Do",
+                ),
+
+                SizedBox(width: 12),
+
+                CardviewComponent(
+                  color: PriorityColor.secondaryColor,
+                  color2: SupportColor.shoulddopb,
+                  task: "0 Task",
+                  title: "Should Do",
+                ),
+
+                SizedBox(width: 12),
+
+                CardviewComponent(
+                  color: PriorityColor.accentColor,
+                  color2: SupportColor.coulddopb,
+                  task: "0 Task",
+                  title: "Could Do",
+                ),
+
+                SizedBox(width: 12),
+              ],
             ),
+          ),
 
-            SpacingComponent(height: 16),
+          SpacingComponent(height: 16),
 
-            CustomText(
-              text: "Today's Tasks", 
-              color: TextColor.primaryTextColor, 
-              weight: FontWeight.w700, 
-              size: 20),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomText(
+                  text: "Today's Tasks",
+                  color: TextColor.primaryTextColor,
+                  weight: FontWeight.w700,
+                  size: 20,
+                ),
+                SpacingComponent(height: 12),
 
-            SpacingComponent(height: 12),
+                CardTaskComponent(
+                  color: PriorityColor.primaryColor,
+                  title: "Belajar Flutter",
+                  desc: "Belajar Component dan layouting Flutter",
+                ),
+              ],
+            ),
+          ),
 
-            CardTaskComponent(color: PriorityColor.primaryColor, title: "Belajar Flutter", desc: "Belajar Component dan layouting Flutter")
+          // Container(
+          //   width: 327,
+          //   height: 117,
+          //   padding: EdgeInsets.all(16),
+          //   decoration: BoxDecoration(
+          //     color: SupportColor.supportColor3,
+          //     borderRadius:BorderRadius.circular(15)
+          //   ),
+          //   child: Column(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //       Row(
+          //         children: [
+          //           CustomText(
+          //             text: "Belajar Flutter",
+          //             color: SupportColor.whiteColor,
+          //             weight: FontWeight.bold,
+          //             size: 22),
 
-            
+          //           SizedBox(width: 113,),
 
-            // Container(
-            //   width: 327,
-            //   height: 117,
-            //   padding: EdgeInsets.all(16),
-            //   decoration: BoxDecoration(
-            //     color: SupportColor.supportColor3,
-            //     borderRadius:BorderRadius.circular(15)
-            //   ),
-            //   child: Column(
-            //     crossAxisAlignment: CrossAxisAlignment.start,
-            //     children: [
-            //       Row(
-            //         children: [
-            //           CustomText(
-            //             text: "Belajar Flutter", 
-            //             color: SupportColor.whiteColor, 
-            //             weight: FontWeight.bold, 
-            //             size: 22),
-                      
-            //           SizedBox(width: 113,),
+          //           const Icon(Icons.more_vert, size: 20, color:SupportColor.whiteColor)
 
-            //           const Icon(Icons.more_vert, size: 20, color:SupportColor.whiteColor)
+          //         ],
+          //       ),
 
-            //         ],
-            //       ),
+          //       SizedBox(height: 6),
 
-            //       SizedBox(height: 6),
+          //       CustomText(
+          //         text: "Belajar Component dan layouting Flutter",
+          //         color: SupportColor.whiteColor,
+          //         weight: FontWeight.w500,
+          //         size: 13),
 
-            //       CustomText(
-            //         text: "Belajar Component dan layouting Flutter", 
-            //         color: SupportColor.whiteColor, 
-            //         weight: FontWeight.w500, 
-            //         size: 13),
+          //       SizedBox(height: 6),
 
-            //       SizedBox(height: 6),
+          //       Text.rich(
+          //         TextSpan(
+          //           children: [
+          //             WidgetSpan(
+          //               alignment: PlaceholderAlignment.middle, // bikin sejajar
+          //               child: Icon(Icons.access_time, size: 16, color: Colors.white),
+          //             ),
+          //         TextSpan(
+          //           text: " 18.30 - 20.30",
+          //           style: TextStyle(color: Colors.white, fontSize: 14),
+          //         ),
+          //           ],
+          //         ),
+          //       )
+          //     ],
+          //   )
 
-            //       Text.rich(
-            //         TextSpan(
-            //           children: [
-            //             WidgetSpan(
-            //               alignment: PlaceholderAlignment.middle, // bikin sejajar
-            //               child: Icon(Icons.access_time, size: 16, color: Colors.white),
-            //             ),
-            //         TextSpan(
-            //           text: " 18.30 - 20.30",
-            //           style: TextStyle(color: Colors.white, fontSize: 14),
-            //         ),
-            //           ],
-            //         ),
-            //       )
-            //     ],
-            //   )
-              
-            // )
-          ],
-        ),
+          // )
+        ],
       ),
     );
   }
