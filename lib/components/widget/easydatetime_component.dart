fdb4241653256c28b7a0232d4a4502ce132fceff
Petrusgld <coysoti@gmail.com>
2025-09-12 06:44:01 +0700

    fix [EM] liisttask-page

---
+++ lib/components/widget/easydatetime_component.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:easy_date_timeline/easy_date_timeline.dart';
+import 'package:ulangan1_11pplg2/components/color/custom_color.dart';
+
+class DateTimelineComponent extends StatelessWidget {
+  const DateTimelineComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return EasyDateTimeLine(
+      initialDate: DateTime.now(),
+      itemBuilder: (context, date, isSelected, onTap) => GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                margin: const EdgeInsets.symmetric(vertical: 8),
+                alignment: Alignment.center,
+                width: 60,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: isSelected ? MainColor.primaryColor : Colors.transparent,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      DateFormat("E").format(date),
+                      style: TextStyle(
+                        color: isSelected ? Colors.white : Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      date.day.toString(),
+                      style: TextStyle(
+                        color: isSelected ? Colors.white : Colors.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                height: 4,
+                width: isSelected ? 24 : 0,
+                decoration: BoxDecoration(
+                  color: isSelected ? MainColor.primaryColor : Colors.transparent,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      timeLineProps: const EasyTimeLineProps(
+        backgroundColor: Colors.transparent,
+      ),
+      locale: 'en_US',
+      dayProps: const EasyDayProps(dayStructure: DayStructure.dayNumDayStr),
+      headerProps: const EasyHeaderProps(
+        dateFormatter: DateFormatter.custom('EEEE d'),
+        showMonthPicker: true,
+        monthPickerType: MonthPickerType.switcher,
+      ),
+    );
+  }
+}
