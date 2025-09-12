872b209100f3b288fefb28b0e1ea815721311b71
Petrusgld <coysoti@gmail.com>
2025-09-12 16:46:55 +0700

    fix [EM] listtask-page

--- lib/components/widget/button_component.dart
+++ lib/components/widget/button_component.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ButtonComponent extends StatelessWidget {
-  final double height;
-  final double width;
+  final double? height;
+  final double? width;
   final double size;
   final double borderRadius;
   final Color color;
-  final Color iconColor;
+  final Color? iconColor;
   final String text;
   final VoidCallback onPressed;
   final FontWeight weight;
@@ -14,59 +14,71 @@ class ButtonComponent extends StatelessWidget {
 
   const ButtonComponent({
     super.key,
-    required this.height,
-    required this.width,
+    this.height,
+    this.width,
     required this.color,
     required this.text,
-    required this.weight,
-    this.borderRadius = 15,
+    this.weight = FontWeight.w500,
+    this.borderRadius = 8,
     required this.onPressed,
-    required this.size,
+    this.size = 18,
     this.icon,
-    required this.iconColor,
-    });
+    this.iconColor,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final shape = RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(borderRadius),
+    );
+
     return SizedBox(
       width: width,
       height: height,
-      child: icon ==null
-
-      ?  ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.all(0),
-          backgroundColor: color,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(borderRadius),
-          ),
-        ),
-        onPressed: onPressed, 
-        child: Text(
-          text,
-          style: TextStyle(fontSize: size, color: Colors.white, fontWeight: weight,),
-        ),
-      )
-
-      : ElevatedButton.icon(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: color,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 14, vertical: 10),
-                  ),
-                  icon: Icon(
-                    Icons.add,
-                    color: iconColor,
-                    size: 24,
-                  ),
-                  label: const Text(
-                    "Add Task",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
+      child: icon == null
+          ? ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: color,
+                shape: shape,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 14,
+                  vertical: 10,
                 ),
+              ),
+              onPressed: onPressed,
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: size,
+                  color: Colors.white,
+                  fontWeight: weight,
+                ),
+              ),
+            )
+          : ElevatedButton.icon(
+              onPressed: onPressed,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: color,
+                shape: shape,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 14,
+                  vertical: 10,
+                ),
+              ),
+              icon: Icon(
+                icon,
+                color: iconColor ?? Colors.white,
+                size: 20,
+              ),
+              label: Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: size,
+                  fontWeight: weight,
+                ),
+              ),
+            ),
     );
   }
 }
