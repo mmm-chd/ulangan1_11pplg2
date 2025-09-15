7afbdbf72cb4f1afc34c2dea93eefd03b8796696
Severus Melvin <melvinmulyono02@gmail.com>
2025-09-15 20:29:50 +0700

    feat [SM] home page

--- lib/components/widget/space_component.dart
+++ lib/components/widget/space_component.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 class SpacingComponent extends StatelessWidget {
-  final double height;
+  final double? height;
+  final double? width;
 
-  const SpacingComponent({
-    super.key, 
-    required this.height,
-    });
+  const SpacingComponent({super.key, this.height, this.width});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(height: height);
+    return SizedBox(height: height, width: width);
   }
 }
