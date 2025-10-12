5f1e84bafe0afd2d43ddf7549e5359e37d95caf2
Petrusgld <coysoti@gmail.com>
2025-10-12 08:13:40 +0700

    feat [EM] login-responsive

---
+++ lib/pages/LoginPage/login_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ulangan1_11pplg2/controller/responsive_controller.dart';
+import 'package:ulangan1_11pplg2/pages/LoginPage/login_page_mobile.dart';
+import 'package:ulangan1_11pplg2/pages/LoginPage/login_page_widescreen.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final controller = Get.find<ResponsiveController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints){
+          controller.updateLayout(constraints);
+          return Obx(
+            ()=> controller.isMobile.value ? LoginPageMobile() : LoginPageWide()
+          );
+        }
+        ),
+    );
+  }
+}
